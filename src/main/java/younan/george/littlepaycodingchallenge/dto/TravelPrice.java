5fb333b9df9b3395fc22217a46ed2bc21c2d571b
georgeyounan <younan.g@gmail.com>
2023-07-28 10:31:10 +1000

    added custom equals method to TravelPrice since direction doesn't matter, will also help with equality checks in tests

--- src/main/java/younan/george/littlepaycodingchallenge/dto/TravelPrice.java
+++ src/main/java/younan/george/littlepaycodingchallenge/dto/TravelPrice.java
@@ -1,5 +1,6 @@
 package younan.george.littlepaycodingchallenge.dto;
 
+import lombok.NonNull;
 import lombok.Value;
 import younan.george.littlepaycodingchallenge.enums.StopId;
 
@@ -7,7 +8,37 @@ import java.math.BigDecimal;
 
 @Value
 public class TravelPrice {
-    private StopId from;
-    private StopId to;
+    @NonNull
+    private StopId stop1;
+    @NonNull
+    private StopId stop2;
+    @NonNull
     private BigDecimal cost;
+
+    /**
+     * Considers objects as equal if cost and stops match, ignoring the direction of stops.
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        TravelPrice other = (TravelPrice) o;
+
+        return this.cost.equals(other.cost) &&
+                ((this.stop1 == other.stop1 && this.stop2 == other.stop2) || (this.stop1 == other.stop2 && this.stop2 == other.stop1));
+
+    }
+
+    /**
+     *
+     * @return a copy of this object where stop 1 becomes stop 2 and vice versa.
+     */
+    public TravelPrice reverseStops() {
+        return new TravelPrice(stop2, stop1, cost);
+    }
 }
